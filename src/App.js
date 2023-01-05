f3abb66da67c0ebaa0009e64475bff50e5a40ba0
Rajaboff <asadbekrajabov3872@gmail.com>
2023-01-05 19:15:29 +0300

    Updated all files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FilterBar from "./components/FilterBar";
+
+function App() {
+  return (
+    <div className="App">
+      <FilterBar />
+    </div>
+  );
+}
+
+export default App;
